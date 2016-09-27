3b17611ac6ca42fde1ced2d6cc98eba3cff29a8b
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2016-09-27 16:26:39 +0200

    Updated ReduceGraph

--- ReduceGraph.java
+++ ReduceGraph.java
@@ -1,22 +1,10 @@
-class Point extends java.awt.Point {
-
-	public Point(int x, int y) {
-
-		super(x, y);
-	}
-
-	public String toString() {
-
-		return String.format("{ %d, %d }", x, y);
-	}
-}
-
 public class ReduceGraph {
+
+	static final int RANGE = 10;
 
 	static public void main( String arg[] ) throws java.io.IOException {
 
-		long seed = Long.parseLong(arg[2]);
-		java.util.Random r = new java.util.Random(seed);
+		java.util.Random r = new java.util.Random(Long.parseLong(arg[2]));
 		final it.unimi.dsi.webgraph.ImmutableGraph graph = it.unimi.dsi.webgraph.ImmutableGraph.load(arg[0]);
 		int n = Integer.parseInt(arg[1]);
 		int nodes = graph.numNodes();
@@ -28,9 +16,7 @@ public class ReduceGraph {
 		int e = 0;
 		int[] g = new int[n * n];
 		java.util.ArrayList<Integer> al = new java.util.ArrayList<Integer>(n);
-		java.util.ArrayList<Point> a = new java.util.ArrayList<Point>(n * n);
-		//a.add(0);
-		//a.add(0);
+		java.util.ArrayList<Integer> a = new java.util.ArrayList<Integer>(n * n);
 		al.add(startNode);
 
 		while (i < n) {
@@ -43,15 +29,14 @@ public class ReduceGraph {
 				int k = al.indexOf(nbrs[j]);
 				if (g[i * n + k] == 0) {
 					g[i * n + k] = g[k * n + i] = ++e;
-					Point node = new Point(i, k);
-					a.add(node);
+					a.add(i);
+					a.add(k);
 				}
 			}
 			i++;
 		}
 
-		String gs = java.util.Arrays.toString(g).replace('[', '{').replace(']', '}');
-		String as = a.toString().replace('[', '{').replace(']', '}');
-		System.out.println("#define N " + n + "\n#define E " + e + "\n#define SEED " + seed + "\nstatic const idc a[" + e + "] = " + as + ";");
+		for (i = 0; i < e; i++)
+			System.out.format("%d %d %d\n", a.get(2 * i), a.get(2 * i + 1), r.nextInt(RANGE * 2) - 14);
 	}
 }
